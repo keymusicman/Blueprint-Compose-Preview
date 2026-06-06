3bdaffd004fd655b61127e6498d2516222a05758
GusWard <gus.ward@the-dots.com>
2026-06-06 14:32:16 +0100

    added noop module contents

---
+++ blueprint-compose-preview-no-op/build.gradle.kts
@@ -0,0 +1,110 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("maven-publish")
+    id("signing")
+}
+
+group = "uk.co.gusward"
+version = "1.0.0"
+
+android {
+    namespace = "uk.co.gusward.blueprint.noop"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 28
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    buildFeatures {
+        compose = true
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "uk.co.gusward"
+            artifactId = "blueprint-compose-preview-no-op"
+            version = "1.0.0"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            pom {
+                name.set("Blueprint Preview No-Op")
+                description.set("No-op version of Blueprint Preview for release builds.")
+                url.set("https://github.com/GusWard/Blueprint-Compose-Preview")
+                
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                
+                developers {
+                    developer {
+                        id.set("gusward")
+                        name.set("Gus Ward")
+                        email.set("guss.warrd@googlemail.com")
+                    }
+                }
+                
+                scm {
+                    connection.set("scm:git:github.com/GusWard/Blueprint-Compose-Preview.git")
+                    developerConnection.set("scm:git:ssh://github.com/GusWard/Blueprint-Compose-Preview.git")
+                    url.set("https://github.com/GusWard/Blueprint-Compose-Preview")
+                }
+            }
+        }
+    }
+    
+    repositories {
+        maven {
+            name = "OSSRH"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = project.findProperty("ossrhUsername") as String?
+                password = project.findProperty("ossrhPassword") as String?
+            }
+        }
+    }
+}
+
+signing {
+    val signingKeyId = project.findProperty("signing.keyId") as String?
+    val signingKey = project.findProperty("signing.key") as String?
+    val signingPassword = project.findProperty("signing.password") as String?
+    
+    if (signingKeyId != null) {
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    }
+    sign(publishing.publications["release"])
+}
+
+dependencies {
+    val composeBom = platform("androidx.compose:compose-bom:2026.05.01")
+    implementation(composeBom)
+    implementation("androidx.compose.runtime:runtime")
+    implementation("androidx.compose.ui:ui")
+}
